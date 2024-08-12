22f2d11de96b8ee7e1164af5a135dbe24ef3c218
Cameron McConnell <cameron@angry-dogman.com>
2024-08-12 13:20:56 -0500

    Finished defining opcodes.
    The opcodes have been defined and included in the CPU_OP_CODES
    static vector.

--- nes_play/src/hardware/opcode.rs
+++ nes_play/src/hardware/opcode.rs
@@ -8,7 +8,7 @@ pub struct Opcode {
 }
 
 enum AddressingModes {
-    Implicit,
+    Implied,
     Accumulator,
     Immediate,
     ZeroPage,
@@ -21,7 +21,6 @@ enum AddressingModes {
     Indirect,
     IndirectX,
     IndirectY,
-    Implied,
 }
 
 impl Opcode {
@@ -38,6 +37,7 @@ impl Opcode {
 
 lazy_static! {
     pub static ref CPU_OP_CODES: Vec<Opcode> = vec![
+        // Add with Carry
         Opcode::new(0x69, "ADC", 2, 2, AddressingModes::Immediate),
         Opcode::new(0x65, "ADC", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0x75, "ADC", 2, 4, AddressingModes::ZeroPageX),
@@ -47,6 +47,7 @@ lazy_static! {
         Opcode::new(0x61, "ADC", 2, 6, AddressingModes::IndirectX),
         Opcode::new(0x71, "ADC", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
         
+        // Logical AND
         Opcode::new(0x29, "AND", 2, 2, AddressingModes::Immediate),
         Opcode::new(0x25, "AND", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0x35, "AND", 2, 4, AddressingModes::ZeroPageX),
@@ -56,41 +57,57 @@ lazy_static! {
         Opcode::new(0x21, "AND", 2, 6, AddressingModes::IndirectX),
         Opcode::new(0x31, "AND", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
 
+        // Arithmetic Shift Left
         Opcode::new(0x0A, "ASL", 1, 2, AddressingModes::Accumulator),
         Opcode::new(0x06, "ASL", 2, 5, AddressingModes::ZeroPage),
         Opcode::new(0x16, "ASL", 2, 6, AddressingModes::ZeroPageX),
         Opcode::new(0x0E, "ASL", 3, 6, AddressingModes::Absolute),
         Opcode::new(0x1E, "ASL", 3, 7, AddressingModes::AbsoluteX),
 
+        // Branch if Carry Clear
         Opcode::new(0x90, "BCC", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Branch if Carry Set
         Opcode::new(0xB0, "BCS", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Branch if Equal
         Opcode::new(0xF0, "BEQ", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Bit Test
         Opcode::new(0x24, "BIT", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0x2c, "BIT", 3, 4, AddressingModes::Absolute),
 
+        // Branch if Minus
         Opcode::new(0x30, "BMI", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Branch if Not Equal
         Opcode::new(0xD0, "BNE", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Branch if Positive
         Opcode::new(0x10, "BPL", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Force Interrupt
         Opcode::new(0x00, "BRK", 1, 7, AddressingModes::Implied),
 
+        // Branch if Overflow Clear
         Opcode::new(0x50, "BVC", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Branch if Overflow Set
         Opcode::new(0x70, "BVS", 2, 2 /*+1 if branch succeeds +2 if to a new page*/, AddressingModes::Relative),
 
+        // Clear Carry Flag
         Opcode::new(0x18, "CLC", 1, 2, AddressingModes::Implied),
 
+        // Clear Decimal Mode
         Opcode::new(0xD8, "CLD", 1, 2, AddressingModes::Implied),
 
+        // Clear Interrupt Disable
         Opcode::new(0x58, "CLI", 1, 2, AddressingModes::Implied),
 
+        // Clear Overflow Flag
         Opcode::new(0xB8, "CLV", 1, 2, AddressingModes::Implied),
 
+        // Compare
         Opcode::new(0xC9, "CMP", 2, 2, AddressingModes::Immediate),
         Opcode::new(0xC5, "CMP", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0xD5, "CMP", 2, 4, AddressingModes::ZeroPageX),
@@ -100,23 +117,29 @@ lazy_static! {
         Opcode::new(0xC1, "CMP", 2, 6, AddressingModes::IndirectX),
         Opcode::new(0xD1, "CMP", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
 
+        // Compare X Register
         Opcode::new(0xE0, "CPX", 2, 2, AddressingModes::Immediate),
         Opcode::new(0xE4, "CPX", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0xEC, "CPX", 3, 4, AddressingModes::Absolute),
 
+        // Compare Y Register
         Opcode::new(0xC0, "CPY", 2, 2, AddressingModes::Immediate),
         Opcode::new(0xC4, "CPY", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0xCC, "CPY", 3, 4, AddressingModes::Absolute),
 
+        // Decrement Memory
         Opcode::new(0xC6, "DEC", 2, 5, AddressingModes::ZeroPage),
         Opcode::new(0xD6, "DEC", 2, 6, AddressingModes::ZeroPageX),
         Opcode::new(0xCE, "DEC", 3, 6, AddressingModes::Absolute),
         Opcode::new(0xDE, "DEC", 3, 7, AddressingModes::AbsoluteX),
 
+        // Decrement X Register
         Opcode::new(0xCA, "DEX", 1, 2, AddressingModes::Implied),
 
+        // Decrement Y Register
         Opcode::new(0x88, "DEY", 1, 2, AddressingModes::Implied),
 
+        // Exclusive OR
         Opcode::new(0x49, "EOR", 2, 2, AddressingModes::Immediate),
         Opcode::new(0x45, "EOR", 2, 3, AddressingModes::ZeroPage),
         Opcode::new(0x55, "EOR", 2, 4, AddressingModes::ZeroPageX),
@@ -126,13 +149,155 @@ lazy_static! {
         Opcode::new(0x41, "EOR", 2, 6, AddressingModes::IndirectX),
         Opcode::new(0x51, "EOR", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
 
+        // Increment Memory
         Opcode::new(0xE6, "INC", 2, 5, AddressingModes::ZeroPage),
         Opcode::new(0xF6, "INC", 2, 6, AddressingModes::ZeroPageX),
         Opcode::new(0xEE, "INC", 3, 6, AddressingModes::Absolute),
         Opcode::new(0xFE, "INC", 3, 7, AddressingModes::AbsoluteX),
 
+        // Increment X Register
         Opcode::new(0xE8, "INX", 1, 2, AddressingModes::Implied),
 
+        // Increment Y Register
         Opcode::new(0xC8, "INY", 1, 2, AddressingModes::Implied),
+
+        // Jump
+        Opcode::new(0x4C, "JMP", 3, 3, AddressingModes::Absolute),
+        Opcode::new(0x6C, "JMP", 3, 5, AddressingModes::Indirect),
+
+        // Jump to Subroutine
+        Opcode::new(0x20, "JSR", 3, 6, AddressingModes::Absolute),
+
+        // Load Accumulator
+        Opcode::new(0xA9, "LDA", 2, 2, AddressingModes::Immediate),
+        Opcode::new(0xA5, "LDA", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0xB5, "LDA", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0xAD, "LDA", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0xBD, "LDA", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteX),
+        Opcode::new(0xB9, "LDA", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteY),
+        Opcode::new(0xA1, "LDA", 2, 6, AddressingModes::IndirectX),
+        Opcode::new(0xB1, "LDA", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
+
+        // Load X Register
+        Opcode::new(0xA2, "LDX", 2, 2, AddressingModes::Immediate),
+        Opcode::new(0xA6, "LDX", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0xB6, "LDX", 2, 4, AddressingModes::ZeroPageY),
+        Opcode::new(0xAE, "LDX", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0xBE, "LDX", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteY),
+
+        // Load Y Register
+        Opcode::new(0xA0, "LDY", 2, 2, AddressingModes::Immediate),
+        Opcode::new(0xA4, "LDY", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0xB4, "LDY", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0xAC, "LDY", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0xBC, "LDY", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteX),
+
+        // Logical Shift Right
+        Opcode::new(0x4A, "LSR", 1, 2, AddressingModes::Accumulator),
+        Opcode::new(0x46, "LSR", 2, 5, AddressingModes::ZeroPage),
+        Opcode::new(0x56, "LSR", 2, 6, AddressingModes::ZeroPageX),
+        Opcode::new(0x4E, "LSR", 3, 6, AddressingModes::Absolute),
+        Opcode::new(0x5E, "LSR", 3, 7, AddressingModes::AbsoluteX),
+
+        // No Operation
+        Opcode::new(0xEA, "NOP", 1, 2, AddressingModes::Implied),
+
+        // Logical Inclusive OR
+        Opcode::new(0x09, "ORA", 2, 2, AddressingModes::Immediate),
+        Opcode::new(0x05, "ORA", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0x15, "ORA", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0x0D, "ORA", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0x1D, "ORA", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteX),
+        Opcode::new(0x19, "ORA", 3, 4 /*+1 if page crossed*/, AddressingModes::AbsoluteY),
+        Opcode::new(0x01, "ORA", 2, 6, AddressingModes::IndirectX),
+        Opcode::new(0x11, "ORA", 2, 5 /*+1 if page crossed*/, AddressingModes::IndirectY),
+
+        // Push Accumulator
+        Opcode::new(0x48, "PHA", 1, 3, AddressingModes::Implied),
+
+        // Push Processor Status
+        Opcode::new(0x08, "PHP", 1, 3, AddressingModes::Implied),
+
+        // Pull Accumulator
+        Opcode::new(0x68, "PLA", 1, 4, AddressingModes::Implied),
+
+        // Pull Processor Status
+        Opcode::new(0x28, "PLP", 1, 4, AddressingModes::Implied),
+
+        // Rotate Left
+        Opcode::new(0x2A, "ROL", 1, 2, AddressingModes::Accumulator),
+        Opcode::new(0x26, "ROL", 2, 5, AddressingModes::ZeroPage),
+        Opcode::new(0x36, "ROL", 2, 6, AddressingModes::ZeroPageX),
+        Opcode::new(0x2E, "ROL", 3, 6, AddressingModes::Absolute),
+        Opcode::new(0x3E, "ROL", 3, 7, AddressingModes::AbsoluteX),
+
+        // Rotate Right
+        Opcode::new(0x6A, "ROR", 1, 2, AddressingModes::Accumulator),
+        Opcode::new(0x66, "ROR", 2, 5, AddressingModes::ZeroPage),
+        Opcode::new(0x76, "ROR", 2, 6, AddressingModes::ZeroPageX),
+        Opcode::new(0x6E, "ROR", 3, 6, AddressingModes::Absolute),
+        Opcode::new(0x7E, "ROR", 3, 7, AddressingModes::AbsoluteX),
+
+        // Return from Interrupt
+        Opcode::new(0x40, "RTI", 1, 6, AddressingModes::Implied),
+
+        // Return from Subroutine
+        Opcode::new(0x60, "RTS", 1, 6, AddressingModes::Implied),
+
+        // Subtract with Carry
+        Opcode::new(0xE9, "SBC", 2, 2, AddressingModes::Immediate),
+        Opcode::new(0xE5, "SBC", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0xF5, "SBC", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0xED, "SBC", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0xFD, "SBC", 3, 4 /*+1 if pages crossed*/, AddressingModes::AbsoluteX),
+        Opcode::new(0xF9, "SBC", 3, 4 /*+1 if pages crossed*/, AddressingModes::AbsoluteY),
+        Opcode::new(0xE1, "SBC", 2, 6, AddressingModes::IndirectX),
+        Opcode::new(0xF1, "SBC", 2, 5 /*+1 if pages crossed*/, AddressingModes::IndirectY),
+
+        // Set Carry Flag
+        Opcode::new(0x38, "SEC", 1, 2, AddressingModes::Implied),
+
+        // Set Decimal Flag
+        Opcode::new(0xF8, "SED", 1, 2, AddressingModes::Implied),
+
+        // Set Interrupt Disable
+        Opcode::new(0x78, "SEI", 1, 2, AddressingModes::Implied),
+
+        // Store Accumulator
+        Opcode::new(0x85, "STA", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0x95, "STA", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0x8D, "STA", 3, 4, AddressingModes::Absolute),
+        Opcode::new(0x9D, "STA", 3, 5, AddressingModes::AbsoluteX),
+        Opcode::new(0x99, "STA", 3, 5, AddressingModes::AbsoluteY),
+        Opcode::new(0x81, "STA", 2, 6, AddressingModes::IndirectX),
+        Opcode::new(0x91, "STA", 2, 6, AddressingModes::IndirectY),
+
+        // Store X Register
+        Opcode::new(0x86, "STX", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0x96, "STX", 2, 4, AddressingModes::ZeroPageY),
+        Opcode::new(0x8E, "STX", 3, 4, AddressingModes::Absolute),
+
+        // Store Y Register
+        Opcode::new(0x84, "STY", 2, 3, AddressingModes::ZeroPage),
+        Opcode::new(0x94, "STY", 2, 4, AddressingModes::ZeroPageX),
+        Opcode::new(0x8C, "STY", 3, 4, AddressingModes::Absolute),
+
+        // Transfer Accumulator to X
+        Opcode::new(0xAA, "TAX", 1, 2, AddressingModes::Implied),
+
+        // Transfer Accumulator to Y
+        Opcode::new(0xA8, "TAY", 1, 2, AddressingModes::Implied),
+
+        // Transfer Stack Pointer to X
+        Opcode::new(0xBA, "TSX", 1, 2, AddressingModes::Implied),
+
+        // Transfer X to Accumulator
+        Opcode::new(0x8A, "TXA", 1, 2, AddressingModes::Implied),
+
+        // Transfer X to Stack Pointer
+        Opcode::new(0x9A, "TXS", 1, 2, AddressingModes::Implied),
+
+        // Transfer Y to Accumulator
+        Opcode::new(0x98, "TYA", 1, 2, AddressingModes::Implied),
     ];
 }
