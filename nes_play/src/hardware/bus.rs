4859822987e5de6b75336ce802d0377e15b4d4bd
Cameron McConnell <cameron@angry-dogman.com>
2024-08-13 09:15:17 -0500

    Added attributes and methods for Bus struct
    These include reset, inc for each clock cycle, read, and just the
    declerations for write and load program. Write needs to implement mirroring
    and for load_program I need to understand the cartridge memory map a bit better
    to implement. Also need to think about how to implement the file reading and how
    that will be triggered in the main loop.

--- nes_play/src/hardware/bus.rs
+++ nes_play/src/hardware/bus.rs
@@ -1,11 +1,49 @@
 pub struct Bus {
-
+    vram: [u8; 2048],
+    cpu_clock_cycle: u64,
+    ppu_clock_cycle: u64,
+    apu_clock_cycle: u64,
 }
 
 impl Bus {
     pub fn new() -> Self {
         Bus {
-        
+            vram: [0; 2048],
+            cpu_clock_cycle: 0,
+            ppu_clock_cycle: 0,
+            apu_clock_cycle: 0,
         }
+    }
+
+    pub fn reset(&mut self) {
+        self.vram = [0; 2048];
+        self.cpu_clock_cycle = 0;
+        self.ppu_clock_cycle = 0;
+        self.apu_clock_cycle = 0;
+    }
+
+    pub fn inc_cpu_clock_cycle(&mut self) {
+        self.cpu_clock_cycle += 1;
+    }
+    
+    pub fn inc_ppu_clock_cycle(&mut self) {
+        self.ppu_clock_cycle += 1;
+    }
+    
+    pub fn inc_apu_clock_cycle(&mut self) {
+        self.apu_clock_cycle += 1;
+    }
+
+    pub fn read(&self, address: u16) -> u8 {
+        self.vram[address as usize]
+    }
+
+    // This needs mirroring
+    pub fn write(&mut self, address: u16, data: u8) {
+        
+    }
+
+    pub fn load_program(&mut self, path: &str) {
+
     }
 }
